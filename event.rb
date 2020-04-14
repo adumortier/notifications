654fefc0d674c156f00cdcf00bb2c83703f6c6ab
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-14 17:34:30 -0600

    Add the info method to produce a hash to convert into json

--- event.rb
+++ event.rb
@@ -9,5 +9,15 @@ class Event
     @end = info[:end].date_time.to_s
     @id = info[:id]
   end 
+
+  def info
+    data = Hash.new
+		data[:name] = @name 
+		data[:description] = @description
+		data[:start] = @start
+		data[:end] = @end
+    data[:id] = @id
+    data
+  end
   
 end
