8cfcde1c15700251adbd5892182a93f9a1b8ca01
Alexis Dumortier <dumortier.alexis@gmail.com>
2020-04-16 09:41:29 -0600

    Remove unused variable

--- google_service.rb
+++ google_service.rb
@@ -51,7 +51,7 @@ class GoogleService < Sinatra::Base
 
   def self.delete_event(token, refresh_token, calendar_id, event_id)
      service = refresh_authorization(token, refresh_token)
-     result = service.delete_event(calendar_id, event_id)
+     service.delete_event(calendar_id, event_id)
   end
 
   def self.get_list_events(token, refresh_token, calendar_id)
